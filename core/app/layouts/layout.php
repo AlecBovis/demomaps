91ab265063027f23b6510c913a50ee1e046ab8d2
AlecBovis <48325823+AlecBovis@users.noreply.github.com>
2019-12-11 03:48:07 -0500

    Update layout.php

--- core/app/layouts/layout.php
+++ core/app/layouts/layout.php
@@ -32,7 +32,6 @@ src="https://maps.googleapis.com/maps/api/js">
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-      <a class="navbar-brand" href="./">MAP<b>CRUD</b></a>
     </div>
 
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
